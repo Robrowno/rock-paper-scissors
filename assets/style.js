150a781785616b338694dad9e076b199e37201b4
Christian Brown <robrowno@icloud.com>
2022-03-31 18:43:23 +0000

    Testing R P S buttons with event listeners and write plan for code

--- assets/style.js
+++ assets/style.js
@@ -1,4 +1,10 @@
+//Game logic
 
+let gameScore = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+}
+//Website opens on startpage 
 let homePage = document.getElementById('start-page');
 homePage.style.display = 'initial';
 
@@ -8,11 +14,58 @@ if (homePage) {
     gamePage.style.display = 'none';
 }
 
+// User can input their name (optional)
+
+
+
+// Game page begins on start button
 function beginGame(e) {
     gamePage.style.display = 'initial';
     homePage.style.display = 'none';
 }
-
 let startGame = document.getElementById('start-game');
 startGame.addEventListener('click', beginGame);
 
+//Game begins when player selects either R, P or S
+
+let rockItm = document.getElementById('rock');
+let paperItm = document.getElementById('paper');
+let scissorsItm = document.getElementById('scissors');
+let playerChoice = [rockItm, paperItm, scissorsItm];
+let computerChoice = ['rock', 'paper', 'scissors'];
+
+
+
+//When player selects Rock
+let chooseRock = rockItm.addEventListener('click', testEvent);
+
+function testEvent() {
+    console.log('This is rock');
+}
+
+
+//When player selects Paper
+let choosePaper = paperItm.addEventListener('click', testEventTwo);
+
+function testEventTwo() {
+    console.log('This is paper');
+}
+
+
+//When player selects Scissors 
+let chooseScissors = scissorsItm.addEventListener('click', testEventThree);
+
+function testEventThree() {
+    console.log('This is scissors');
+}
+
+
+
+
+//Sequence when player wins or loses
+
+
+//Score tallies.
+
+
+//Reset scoreboard 
